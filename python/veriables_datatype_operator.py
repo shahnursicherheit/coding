5f78af188a4b8e4b801e3bf9431c7a598f0d652c
shahnursicherheit <mdshahnur05@gmail.com>
2025-01-31 01:06:31 +0600

    python starting from zero

---
+++ python/veriables_datatype_operator.py
@@ -0,0 +1,48 @@
+# veriable data type and operator in this code
+
+# prit somthing using print()
+print("good to see this good ")
+
+# veriable in python their is no need to use any command for creating veriable
+
+
+intege = float(5) 
+floatin = int(3.3) 
+characte = 'a'
+strin = "string"
+
+print(intege)
+print(floatin)
+print(characte)
+print(strin)
+
+# python veriable are  not case sensitive 
+
+a = 1 
+A = 3 
+print (a)
+print (A)
+
+
+# python allow declare  multiple in once by comma 
+
+b, c, d = 1, '2', "3"
+
+print ( b )
+print ( c )
+print ( d )
+
+
+B = C = D = 'nothing'
+
+print ( b, c, d )
+
+print ( B, C, D )
+
+# + operator can add multiple variables as a concatination 
+
+a = 'i '
+b = 'am'
+c = ' nobe'
+
+print ( a+b+c, B )
